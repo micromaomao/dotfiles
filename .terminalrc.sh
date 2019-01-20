773cb1d6a520d47c9994c6702bbabe6f0c3cb106
Mao Wtm <micromaomao@gmail.com>
2019-01-20 21:08:31 +0800

    Use ts-player for recording from now on.

--- .terminalrc.sh
+++ .terminalrc.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [ -t 0 -a -t 1 -a -z "$TYPESCRIPT_REC" ]; then
+if [ -t 0 -a -t 1 -a -z "$ITS_REC" ]; then
   time=$(date +%Y%m%d_%H%M%S)
-  export TYPESCRIPT_REC="$HOME/terminal-records/$time.typescript"
-  export TYPESCRIPT_REC_TIMEFILE="$HOME/terminal-records/$time.typescript.tl"
-  exec script -c $HOME/.terminalrc.sh -f -e -t$TYPESCRIPT_REC_TIMEFILE $TYPESCRIPT_REC
+  export ITS_REC="$HOME/terminal-records/$time.its"
+  exec ts-player record $ITS_REC
 fi
 
-if tmux has; then tmux a; else tmux; fi
