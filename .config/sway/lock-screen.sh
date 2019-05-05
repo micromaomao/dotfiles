133874ba8b310db737e7b88975dd7b0d3202e5a3
Mao Wtm <m@maowtm.org>
2019-05-05 21:16:15 +0800

    Update lock-screen.sh

--- .config/sway/lock-screen.sh
+++ .config/sway/lock-screen.sh
@@ -4,5 +4,5 @@ FILE="/tmp/Screenshot-$(date +%Y%m%d_%H%M%S).png"
 grim -t png "$FILE" && \
 LOCKIMAGE="$(mktemp /tmp/lock-XXXXXXXXXXX.png)"
 convert "$FILE" -virtual-pixel transparent -background white -filter Gaussian -resize 20% -define "filter:sigma=2" \
-  -resize 500.5% -fill 'rgba(255,255,255,0.5)' -draw 'rectangle 0,0 10000,10000' "$LOCKIMAGE"
+  -resize 500.5% -fill 'rgba(0,0,0,0.5)' -draw 'rectangle 0,0 10000,10000' "$LOCKIMAGE"
 swaylock --daemonize -i "$LOCKIMAGE"
