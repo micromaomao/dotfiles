dc1638f1b98af9eb1ba3775e3f35679e6d6b1111
Mao <m@maowtm.org>
2023-12-03 00:08:38 +0000

    .

--- .config/sway/take-screenshot.sh
+++ .config/sway/take-screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-FILE="/tmp/Screenshot-$(date +%Y%m%d_%H%M%S).png"
+FILE="/home/mao/Pictures/Screenshot from $(date '+%Y-%m-%d %H-%M-%S').png"
 grim -t png "$FILE" && \
 cat $FILE | wl-copy --type image/png
