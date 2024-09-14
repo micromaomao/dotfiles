c722e95e656b7d46a2f880b1c2339f607a905499
Mao <m@maowtm.org>
2024-09-14 17:51:22 +0100

    Update dotfiles script

--- dotfiles.mww.moe/install.sh
+++ dotfiles.mww.moe/install.sh
@@ -9,16 +9,21 @@ fi
 
 if [ -f /etc/os-release ]; then
   . /etc/os-release
-  echo Detected debian $VERSION_ID
+  echo Detected $ID $VERSION_ID
 
-  set -xe
-  sudo apt update
-  sudo apt install -y git curl tmux fish exa htop kitty-terminfo
-  set +xe
+  if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
+    set -xe
+    sudo apt update
+    sudo apt install -y git curl tmux fish exa htop kitty-terminfo
+    set +xe
+  else
+    echo "Unsupported OS."
+    exit 1
+  fi
 
   type docker > /dev/null
   if [ $? -ne 0 ]; then
-    if [ $VERSION_ID -eq 11 -o $VERSION_ID -eq 10 ]; then
+    if [[ $ID == "debian" ]] && [ $VERSION_ID -eq 11 -o $VERSION_ID -eq 10 ]; then
       read -p "Install docker? (y/n) " -r P
       if [ $P = "y" ]; then
         set -xe
