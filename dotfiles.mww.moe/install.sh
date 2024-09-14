e579f066e0fa849aacf73a62b5228a94ea3acd0e
Mao <m@maowtm.org>
2024-09-14 18:18:26 +0100

    Update

--- dotfiles.mww.moe/install.sh
+++ dotfiles.mww.moe/install.sh
@@ -23,7 +23,7 @@ if [ -f /etc/os-release ]; then
 
   type docker > /dev/null
   if [ $? -ne 0 ]; then
-    if [ $ID = "debian" ] && [ $VERSION_ID -eq 11 -o $VERSION_ID -eq 10 ]; then
+    if [ $ID = "debian" ]; then
       read -p "Install docker? (y/n) " -r P
       if [ $P = "y" ]; then
         set -xe
@@ -35,7 +35,24 @@ if [ -f /etc/os-release ]; then
           $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo chmod a+r /etc/apt/sources.list.d/docker.list
         sudo apt-get update
-        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        set +xe
+      fi
+    fi
+    if [ $ID = "ubuntu" ]; then
+      read -p "Install docker? (y/n) " -r P
+      if [ $P = "y" ]; then
+        set -xe
+        sudo apt-get install ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         set +xe
       fi
     fi
